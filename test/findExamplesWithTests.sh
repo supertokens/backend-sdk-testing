c4bcd00392fd8a4f1fff008b6f278d1305af37b8
Sandro de Souza <sandesou@gmail.com>
2024-06-03 15:11:56 -0300

    clean up out-of-scope tests

--- test/findExamplesWithTests.sh
+++
@@ -1,9 +0,0 @@
-matrixEntries=""
-delim=""
-while IFS=  read -r -d $'\0'; do
-    matrixEntries+=$delim"{\"examplePath\": \""$(echo "$REPLY" | sed 's/\/test$//' )\""}";       # or however you want to process each file
-    delim=", "
-done < <(find ./examples -name node_modules -prune -or -path "*/test" -print0)
-
-matrixEntries="[$matrixEntries]"
-echo $matrixEntries
