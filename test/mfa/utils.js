a49c79603412992fbbd89794d69301ec05e4ca6c
rishabhpoddar <rishabh.poddar@gmail.com>
2024-06-24 23:17:49 +0530

    adds first mfa test

---
+++ test/mfa/utils.js
@@ -0,0 +1,274 @@
+/* Copyright (c) 2024, VRAI Labs and/or its affiliates. All rights reserved.
+ *
+ * This software is licensed under the Apache License, Version 2.0 (the
+ * "License") as published by the Apache Software Foundation.
+ *
+ * You may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+const { recipesMock, request } = require("../../api-mock");
+const { EmailVerification, Passwordless, supertokens } = recipesMock;
+
+module.exports.epSignUp = async function (email, password, accessToken) {
+    if (accessToken === undefined) {
+        return await new Promise((resolve) => {
+            request()
+                .post("/auth/signup")
+                .send({
+                    formFields: [
+                        {
+                            id: "password",
+                            value: password,
+                        },
+                        {
+                            id: "email",
+                            value: email,
+                        },
+                    ],
+                })
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        resolve(undefined);
+                    } else {
+                        resolve(res);
+                    }
+                })
+        });
+    } else {
+        return await new Promise((resolve) => {
+            request()
+                .post("/auth/signup")
+                .set("Authorization", `Bearer ${accessToken}`)
+                .send({
+                    formFields: [
+                        {
+                            id: "password",
+                            value: password,
+                        },
+                        {
+                            id: "email",
+                            value: email,
+                        },
+                    ],
+                })
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        resolve(undefined);
+                    } else {
+                        resolve(res);
+                    }
+                })
+        });
+    }
+};
+
+module.exports.validateUserEmail = async (id) => {
+    return EmailVerification.verifyEmailUsingToken(
+        "public",
+        (await EmailVerification.createEmailVerificationToken("public", supertokens.convertToRecipeUserId(id))).token,
+        false
+    );
+};
+
+module.exports.epSignIn = async function (email, password, accessToken) {
+    if (accessToken === undefined) {
+        return request()
+            .post("/auth/signin")
+            .send({
+                formFields: [
+                    {
+                        id: "password",
+                        value: password,
+                    },
+                    {
+                        id: "email",
+                        value: email,
+                    },
+                ],
+            })
+    } else {
+        return request()
+            .post("/auth/signin")
+            .set("Authorization", `Bearer ${accessToken}`)
+            .send({
+                formFields: [
+                    {
+                        id: "password",
+                        value: password,
+                    },
+                    {
+                        id: "email",
+                        value: email,
+                    },
+                ],
+            });
+    }
+};
+
+module.exports.plessCreateCode = async function ({ email, phoneNumber }, accessToken) {
+    if (accessToken === undefined) {
+        return request().post("/auth/signinup/code").send({
+            email,
+            phoneNumber,
+        });
+    } else {
+        return request()
+            .post("/auth/signinup/code")
+            .set("Authorization", `Bearer ${accessToken}`)
+            .send({
+                email,
+                phoneNumber,
+            })
+            .expect(200);
+    }
+};
+
+module.exports.plessResendCode = async function (code, accessToken) {
+    if (accessToken === undefined) {
+        return request().post("/auth/signinup/code/resend").send({
+            preAuthSessionId: code.preAuthSessionId,
+            deviceId: code.deviceId,
+        });
+    } else {
+        return request()
+            .post("/auth/signinup/code/resend")
+            .set("Authorization", `Bearer ${accessToken}`)
+            .send({
+                preAuthSessionId: code.preAuthSessionId,
+                deviceId: code.deviceId,
+            })
+            .expect(200);
+    }
+};
+
+module.exports.plessEmailSignInUp = async function (email, accessToken) {
+    const code = await Passwordless.createCode({
+        tenantId: "public",
+        email,
+    });
+
+    if (accessToken === undefined) {
+        return await new Promise((resolve) => {
+            request().post("/auth/signinup/code/consume").send({
+                preAuthSessionId: code.preAuthSessionId,
+                userInputCode: code.userInputCode,
+                deviceId: code.deviceId,
+            })
+                .end((err, res) => {
+                    if (err) {
+                        resolve(undefined);
+                    } else {
+                        resolve(res);
+                    }
+                })
+        });
+    } else {
+        return await new Promise((resolve) => {
+            request()
+                .post("/auth/signinup/code/consume")
+                .set("Authorization", `Bearer ${accessToken}`)
+                .send({
+                    preAuthSessionId: code.preAuthSessionId,
+                    userInputCode: code.userInputCode,
+                    deviceId: code.deviceId,
+                })
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        resolve(undefined);
+                    } else {
+                        resolve(res);
+                    }
+                })
+        });
+    }
+};
+
+module.exports.plessPhoneSigninUp = async function (phoneNumber, accessToken) {
+    const code = await Passwordless.createCode({
+        tenantId: "public",
+        phoneNumber,
+    });
+
+    if (accessToken === undefined) {
+        return request().post("/auth/signinup/code/consume").send({
+            preAuthSessionId: code.preAuthSessionId,
+            userInputCode: code.userInputCode,
+            deviceId: code.deviceId,
+        });
+    } else {
+        return request().post("/auth/signinup/code/consume").set("Authorization", `Bearer ${accessToken}`).send({
+            preAuthSessionId: code.preAuthSessionId,
+            userInputCode: code.userInputCode,
+            deviceId: code.deviceId,
+        });
+    }
+};
+
+module.exports.tpSignInUp = async function (thirdPartyId, email, accessToken) {
+    if (accessToken === undefined) {
+        return await new Promise((resolve) => {
+            request()
+                .post("/auth/signinup")
+                .send({
+                    thirdPartyId: thirdPartyId,
+                    redirectURIInfo: {
+                        redirectURIOnProviderDashboard: "http://127.0.0.1/callback",
+                        redirectURIQueryParams: {
+                            email: email,
+                        },
+                    },
+                })
+                .end((err, res) => {
+                    if (err) {
+                        resolve(undefined);
+                    } else {
+                        resolve(res);
+                    }
+                })
+        });
+    } else {
+        return await new Promise((resolve) => {
+            request()
+                .post("/auth/signinup")
+                .set("Authorization", `Bearer ${accessToken}`)
+                .send({
+                    thirdPartyId: thirdPartyId,
+                    redirectURIInfo: {
+                        redirectURIOnProviderDashboard: "http://127.0.0.1/callback",
+                        redirectURIQueryParams: {
+                            email: email,
+                        },
+                    },
+                })
+                .end((err, res) => {
+                    if (err) {
+                        resolve(undefined);
+                    } else {
+                        resolve(res);
+                    }
+                })
+        })
+    }
+};
+
+module.exports.getMfaInfo = async function (accessToken, statusCode = 200) {
+    return request().put("/auth/mfa/info").set("Authorization", `Bearer ${accessToken}`).expect(statusCode);
+};
+
+exports.getTestEmail = function getTestEmail(suffix) {
+    return `john.doe+${Date.now()}+${suffix ?? 1}@supertokens.io`;
+};
+
+exports.getTestPhoneNumber = function () {
+    return `+3630${Date.now().toString().substr(-7)}`;
+};
