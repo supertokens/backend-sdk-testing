572b790ba190a126d8f0332f0723b75efd1f9a10
Ankit Tiwari <ankucodes@gmail.com>
2024-07-08 13:18:39 +0530

    feat: Add OAuth2Client tests and refactor OAuth2 tests

--- test/oauth2/oauth2.api.test.js
+++ test/oauth2/oauth2.api.test.js
@@ -15,9 +15,9 @@
 
 const { printPath, setupST, startST: globalStartST, killAllST, cleanST, createTenant } = require("../utils");
 let assert = require("assert");
-const { recipesMock, getOverrideParams, randomString, API_PORT } = require("../../api-mock");
+const { recipesMock, randomString, API_PORT } = require("../../api-mock");
 const { OAuth2, EmailPassword, Session, supertokens: SuperTokens } = recipesMock;
-const setCookieParser = require("set-cookie-parser");
+const { createAuthorizationUrl, testOAuthFlowAndGetAuthCode } = require("../oauth2/utils");
 
 describe(`OAuth2-API: ${printPath("[test/oauth2/oauth2.api.test.js]")}`, function () {
     let globalConnectionURI;
@@ -42,6 +42,7 @@ describe(`OAuth2-API: ${printPath("[test/oauth2/oauth2.api.test.js]")}`, functio
 
         const apiDomain = `http://localhost:${API_PORT}`;
         const websiteDomain = "http://supertokens.io";
+        const scope = "profile";
 
         SuperTokens.init({
             supertokens: {
@@ -55,24 +56,11 @@ describe(`OAuth2-API: ${printPath("[test/oauth2/oauth2.api.test.js]")}`, functio
             recipeList: [EmailPassword.init(), OAuth2.init(), Session.init()],
         });
 
-        // This will be used to store all Set-Cookie headers in the subsequent requests
-        const allSetCookieHeaders = [];
-
-        function saveCookies(res) {
-            const cookieStr = setCookieParser.splitCookiesString(res.headers.get("Set-Cookie"));
-            const cookies = setCookieParser.parse(cookieStr);
-            allSetCookieHeaders.push(...cookies);
-        }
-
-        function getCookieHeader() {
-            return allSetCookieHeaders.map((cookie) => `${cookie.name}=${cookie.value}`).join("; ");
-        }
-
         const redirectUri = "http://localhost:4000/redirect-url";
         const { client } = await OAuth2.createOAuth2Client(
             {
                 redirectUris: [redirectUri],
-                scope: "profile",
+                scope,
                 skipConsent: true,
                 grantTypes: ["authorization_code", "refresh_token"],
                 responseTypes: ["code", "id_token"],
@@ -83,108 +71,33 @@ describe(`OAuth2-API: ${printPath("[test/oauth2/oauth2.api.test.js]")}`, functio
 
         const state = new Buffer.from("some-random-string", "base64").toString();
 
-        const queryParams = {
-            client_id: client.clientId,
-            redirect_uri: redirectUri,
-            response_type: "code",
-            scope: "profile",
-            state,
-        };
+        const authorisationUrl = createAuthorizationUrl({ apiDomain, clientId : client.clientId, redirectUri, state });
 
-        // Start the OAuth Flow
-        let url = `${apiDomain}/auth/oauth2/auth?${new URLSearchParams(queryParams)}`;
-        let res = await fetch(url, { method: "GET", redirect: "manual" });
-        saveCookies(res);
-
-        let nextUrl = res.headers.get("Location");
-        let nextUrlObj = new URL(nextUrl);
-        const loginChallenge = nextUrlObj.searchParams.get("login_challenge");
-
-        assert.strictEqual(nextUrlObj.origin + nextUrlObj.pathname, apiDomain + "/auth/oauth2/login");
-        assert(loginChallenge !== null);
-
-        res = await fetch(nextUrl, {
-            method: "GET",
-            redirect: "manual",
-            headers: {
-                Cookie: getCookieHeader(),
-            },
+        const { authorizationCode } = await testOAuthFlowAndGetAuthCode({
+            apiDomain,
+            websiteDomain,
+            authorisationUrl,
+            clientId: client.clientId,
+            redirectUri,
+            scope,
+            state
         });
 
-        saveCookies(res);
-
-        nextUrl = res.headers.get("Location");
-        nextUrlObj = new URL(nextUrl);
-
-        assert.strictEqual(nextUrlObj.origin + nextUrlObj.pathname, `${websiteDomain}/auth`);
-        assert.strictEqual(nextUrlObj.searchParams.get("loginChallenge"), loginChallenge);
-
-
-        // We have been redirected to the frontend login page. We will create the session manually to simulate login.
-        const createSessionUser = await EmailPassword.signUp("public", "test@example.com", "password123");
-        const session = await Session.createNewSessionWithoutRequestResponse("public", createSessionUser.recipeUserId);
-
-        res = await fetch(`${apiDomain}/auth/oauth2/login?loginChallenge=${loginChallenge}`, {
-            method: "GET",
-            redirect: "manual",
-            headers: {
-                Authorization: `Bearer ${session.getAccessToken()}`,
-                Cookie: getCookieHeader(),
-            },
+        const res = await fetch(`${apiDomain}/auth/oauth2/token`, {
+            method: "POST",
+            body: new URLSearchParams({
+                code: authorizationCode,
+                client_id: client.clientId,
+                client_secret: client.clientSecret,
+                grant_type: "authorization_code",
+                redirect_uri: redirectUri,
+            }),
         });
+        const tokenResp = await res.json();
 
-        saveCookies(res);
-
-        nextUrl = res.headers.get("Location");
-        nextUrlObj = new URL(nextUrl);
-
-        assert.strictEqual(nextUrlObj.origin + nextUrlObj.pathname, `${apiDomain}/auth/oauth2/auth`);
-        assert.strictEqual(nextUrlObj.searchParams.get("client_id"), client.clientId);
-        assert.strictEqual(nextUrlObj.searchParams.get("redirect_uri"), redirectUri);
-        assert.strictEqual(nextUrlObj.searchParams.get("response_type"), "code");
-        assert.strictEqual(nextUrlObj.searchParams.get("scope"), "profile");
-        assert.strictEqual(nextUrlObj.searchParams.get("state"), state);
-        assert(nextUrlObj.searchParams.get("login_verifier") !== null);
-
-        res = await fetch(nextUrl, {
-            method: "GET",
-            redirect: "manual",
-            headers: {
-                Authorization: `Bearer ${session.getAccessToken()}`,
-                Cookie: getCookieHeader(),
-            },
-        });
-
-        saveCookies(res);
-
-        nextUrl = res.headers.get("Location");
-        nextUrlObj = new URL(nextUrl);
-
-        assert.strictEqual(nextUrlObj.origin + nextUrlObj.pathname, `${apiDomain}/auth/oauth2/auth`);
-        assert.strictEqual(nextUrlObj.searchParams.get("client_id"), client.clientId);
-        assert.strictEqual(nextUrlObj.searchParams.get("redirect_uri"), redirectUri);
-        assert.strictEqual(nextUrlObj.searchParams.get("response_type"), "code");
-        assert.strictEqual(nextUrlObj.searchParams.get("scope"), "profile");
-        assert.strictEqual(nextUrlObj.searchParams.get("state"), state);
-        assert(nextUrlObj.searchParams.get("consent_verifier") !== null);
-
-
-        res = await fetch(nextUrl, {
-            method: "GET",
-            redirect: "manual",
-            headers: {
-                Authorization: `Bearer ${session.getAccessToken()}`,
-                Cookie: getCookieHeader(),
-            },
-        });
-
-        saveCookies(res);
-
-        nextUrl = res.headers.get("Location");
-        nextUrlObj = new URL(nextUrl);
-
-        assert.strictEqual(nextUrlObj.origin + nextUrlObj.pathname, redirectUri);
-        assert.strictEqual(nextUrlObj.searchParams.get("scope"), "profile");
-        assert.strictEqual(nextUrlObj.searchParams.get("state"), state);
+        assert.strictEqual(res.status, 200);
+        assert(tokenResp.access_token !== undefined);
+        assert.strictEqual(tokenResp.token_type, "bearer");
+        assert.strictEqual(tokenResp.scope, scope);
     });
 });
---
+++ test/oauth2/utils.js
@@ -0,0 +1,152 @@
+const assert = require("assert");
+const setCookieParser = require("set-cookie-parser");
+const { request, recipesMock } = require("../../api-mock");
+const { EmailPassword, Session } = recipesMock;
+
+exports.getAuthorizationUrlFromAPI = async function ({ redirectUri, scope, state }) {
+  const response = await new Promise((resolve) =>
+    request()
+      .get(`/auth/oauth2client/authorisationurl?redirectURIOnProviderDashboard=${redirectUri}`)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          resolve(undefined);
+        } else {
+          resolve(res);
+        }
+      })
+  );
+  assert(response !== undefined);
+  const authUrl = new URL(response.body.urlWithQueryParams);
+
+  let authUrlObj = new URL(authUrl);
+
+  authUrlObj.searchParams.set("scope", scope);
+  authUrlObj.searchParams.set("state", state);
+  return authUrlObj.toString();
+};
+
+exports.createAuthorizationUrl = function ({ apiDomain, clientId, redirectUri, state, extraQueryParams = {}}) {
+  const queryParams = {
+    client_id: clientId,
+    redirect_uri: redirectUri,
+    response_type: "code",
+    scope: "profile",
+    state,
+    ...extraQueryParams,
+};
+
+  return `${apiDomain}/auth/oauth2/auth?${new URLSearchParams(queryParams)}`;
+}
+
+exports.testOAuthFlowAndGetAuthCode = async function ({ apiDomain, websiteDomain, clientId, authorisationUrl, redirectUri, scope, state, useSignIn = false }) {
+    // This will be used to store all Set-Cookie headers in the subsequent requests
+    const allSetCookieHeaders = [];
+
+    function saveCookies(res) {
+        const cookieStr = setCookieParser.splitCookiesString(res.headers.get("Set-Cookie"));
+        const cookies = setCookieParser.parse(cookieStr);
+        allSetCookieHeaders.push(...cookies);
+    }
+
+    function getCookieHeader() {
+        return allSetCookieHeaders.map((cookie) => `${cookie.name}=${cookie.value}`).join("; ");
+    }
+
+    // Start the OAuth Flow
+    let res = await fetch(authorisationUrl, { method: "GET", redirect: "manual" });
+    saveCookies(res);
+
+    let nextUrl = res.headers.get("Location");
+    let nextUrlObj = new URL(nextUrl);
+    const loginChallenge = nextUrlObj.searchParams.get("login_challenge");
+
+    assert.strictEqual(nextUrlObj.origin + nextUrlObj.pathname, apiDomain + "/auth/oauth2/login");
+    assert(loginChallenge !== null);
+
+    res = await fetch(nextUrl, {
+        method: "GET",
+        redirect: "manual",
+        headers: {
+            Cookie: getCookieHeader(),
+        },
+    });
+
+    saveCookies(res);
+
+    nextUrl = res.headers.get("Location");
+    nextUrlObj = new URL(nextUrl);
+
+    assert.strictEqual(nextUrlObj.origin + nextUrlObj.pathname, `${websiteDomain}/auth`);
+    assert.strictEqual(nextUrlObj.searchParams.get("loginChallenge"), loginChallenge);
+
+    // We have been redirected to the frontend login page. We will create the session manually to simulate login.
+    const createSessionUser = useSignIn ? await EmailPassword.signIn("public", "test@example.com", "password123") : await EmailPassword.signUp("public", "test@example.com", "password123");
+
+    const session = await Session.createNewSessionWithoutRequestResponse("public", createSessionUser.recipeUserId);
+
+    res = await fetch(`${apiDomain}/auth/oauth2/login?loginChallenge=${loginChallenge}`, {
+        method: "GET",
+        redirect: "manual",
+        headers: {
+            Authorization: `Bearer ${session.getAccessToken()}`,
+            Cookie: getCookieHeader(),
+        },
+    });
+
+    saveCookies(res);
+
+    nextUrl = res.headers.get("Location");
+    nextUrlObj = new URL(nextUrl);
+
+    assert.strictEqual(nextUrlObj.origin + nextUrlObj.pathname, `${apiDomain}/auth/oauth2/auth`);
+    assert.strictEqual(nextUrlObj.searchParams.get("client_id"), clientId);
+    assert.strictEqual(nextUrlObj.searchParams.get("redirect_uri"), redirectUri);
+    assert.strictEqual(nextUrlObj.searchParams.get("response_type"), "code");
+    assert.strictEqual(nextUrlObj.searchParams.get("scope"), scope);
+    assert.strictEqual(nextUrlObj.searchParams.get("state"), state);
+    assert(nextUrlObj.searchParams.get("login_verifier") !== null);
+
+    res = await fetch(nextUrl, {
+        method: "GET",
+        redirect: "manual",
+        headers: {
+            Authorization: `Bearer ${session.getAccessToken()}`,
+            Cookie: getCookieHeader(),
+        },
+    });
+
+    saveCookies(res);
+
+    nextUrl = res.headers.get("Location");
+    nextUrlObj = new URL(nextUrl);
+
+    assert.strictEqual(nextUrlObj.origin + nextUrlObj.pathname, `${apiDomain}/auth/oauth2/auth`);
+    assert.strictEqual(nextUrlObj.searchParams.get("client_id"), clientId);
+    assert.strictEqual(nextUrlObj.searchParams.get("redirect_uri"), redirectUri);
+    assert.strictEqual(nextUrlObj.searchParams.get("response_type"), "code");
+    assert.strictEqual(nextUrlObj.searchParams.get("scope"), scope);
+    assert.strictEqual(nextUrlObj.searchParams.get("state"), state);
+    assert(nextUrlObj.searchParams.get("consent_verifier") !== null);
+
+    res = await fetch(nextUrl, {
+        method: "GET",
+        redirect: "manual",
+        headers: {
+            Authorization: `Bearer ${session.getAccessToken()}`,
+            Cookie: getCookieHeader(),
+        },
+    });
+
+    saveCookies(res);
+
+    nextUrl = res.headers.get("Location");
+    nextUrlObj = new URL(nextUrl);
+    const authorizationCode = nextUrlObj.searchParams.get("code");
+
+    assert.strictEqual(nextUrlObj.origin + nextUrlObj.pathname, redirectUri);
+    assert.strictEqual(nextUrlObj.searchParams.get("scope"), scope);
+    assert.strictEqual(nextUrlObj.searchParams.get("state"), state);
+
+    return { authorizationCode, userId: session.getUserId()};
+}
