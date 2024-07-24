75c0b5ba14b3bcd59bfe24107019953439670446
Sattvik Chakravarthy <sattvik@gmail.com>
2024-07-24 15:41:41 +0530

    fix: thirdparty tests

--- api-mock/mocks/MultitenancyMock.ts
+++ api-mock/mocks/MultitenancyMock.ts
@@ -15,10 +15,52 @@ export const MultitenancyMock: Partial<typeof Multitenancy> = {
             input: { tenantId, config, userContext },
         });
     },
+    getTenant: async (tenantId, userContext) => {
+        return await queryAPI({
+            method: "post",
+            path: "/test/multitenancy/gettenant",
+            input: { tenantId, userContext },
+        });
+    },
     associateUserToTenant: async (tenantId, recipeUserId, userContext) => {
         return await queryAPI({
             method: "post",
             path: "/test/multitenancy/associateusertotenant",
+            input: { tenantId, recipeUserId: recipeUserId.getAsString(), userContext },
+        });
+    },
+    deleteTenant: async (tenantId, userContext) => {
+        return await queryAPI({
+            method: "post",
+            path: "/test/multitenancy/deletetenant",
+            input: { tenantId, userContext },
+        });
+    },
+    listAllTenants: async (userContext) => {
+        return await queryAPI({
+            method: "post",
+            path: "/test/multitenancy/listalltenants",
+            input: { userContext },
+        });
+    },
+    createOrUpdateThirdPartyConfig: async (tenantId, config, skipValidation, userContext) => {
+        return await queryAPI({
+            method: "post",
+            path: "/test/multitenancy/createorupdatethirdpartyconfig",
+            input: { tenantId, config, skipValidation, userContext },
+        });
+    },
+    deleteThirdPartyConfig: async (tenantId, thirdPartyId, userContext) => {
+        return await queryAPI({
+            method: "post",
+            path: "/test/multitenancy/deletethirdpartyconfig",
+            input: { tenantId, thirdPartyId, userContext },
+        });
+    },
+    disassociateUserFromTenant: async (tenantId, recipeUserId, userContext) => {
+        return await queryAPI({
+            method: "post",
+            path: "/test/multitenancy/disassociateuserfromtenant",
             input: { tenantId, recipeUserId: recipeUserId.getAsString(), userContext },
         });
     },
